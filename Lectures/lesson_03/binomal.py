1641b186a16d51556270638b6e26e5bce3537203
SimchaTeich <sim.teich@gmail.com>
2022-11-08 22:36:00 +0200

    document the code

--- Lectures/lesson_03/binomal.py
+++ Lectures/lesson_03/binomal.py
@@ -28,40 +28,40 @@ def regular_binomal(n, k):
     return regular_binomal(n-1, k) + regular_binomal(n-1, k-1)
 
 
-def print_board(b):
-    for line in b:
-        print(" ".join([str(item) for item in line]))
-
-
 def dynamic_binomal(n, k):
+    """
+:   calcs the binomal number (n choose k)
+:   by the dynamic programming way.
+:
+:   time complex: O(n^2)
+:
+:   param n: size of group to choose from it
+:   type n: int
+:   param k: who many member to choose from the group.
+:   type k: int
+    """
     # build matrix nXn
     values = [['.' for _ in range(n+1)] for _ in range(n+1)]
     
     for i in range(0,n+1):
         for j in range(0, i+1):
-            
             if j == 0 or j == i:
                 values[i][j] = 1
             
             else:
                 values[i][j] = values[i-1][j-1] + values[i-1][j]
             
-            
             if i == n and j == k:
-                return values[i][j], values
+                return values[i][j]
     
-
 
 def main():
     print("calc with dynamic programing (30 choose 20) == ")
-    answer, board = dynamic_binomal(30, 20)
-    print(answer)
-    #print_board(board)
+    print(dynamic_binomal(30, 20))
     
     
     print("calc with regular recursive (30 choose 20) == ")
-    answer = regular_binomal(30, 20)
-    print(answer)
+    print(regular_binomal(30, 20))
     
     
     
