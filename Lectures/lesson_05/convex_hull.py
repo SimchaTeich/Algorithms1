b2462b37e7836fadf60d0bd7fc861841fa0c8392
SimchaTeich <sim.teich@gmail.com>
2022-11-27 13:47:47 +0200

    implement the convex hull allgo

--- Lectures/lesson_05/convex_hull.py
+++ Lectures/lesson_05/convex_hull.py
@@ -42,9 +42,9 @@ class Board:
 
     
     def _determinant_3x3(self, matrix):
-        return matrix[0][0] * determinant_2x2([matrix[1][1:], matrix[2][1:]]) -\
-            matrix[0][1] * determinant_2x2([[matrix[1][0], matrix[1][2]], [matrix[2][0], matrix[2][2]]]) +\
-            matrix[0][2] * determinant_2x2([matrix[1][:-1], matrix[2][:-1]])
+        return matrix[0][0] * self._determinant_2x2([matrix[1][1:], matrix[2][1:]]) -\
+            matrix[0][1] * self._determinant_2x2([[matrix[1][0], matrix[1][2]], [matrix[2][0], matrix[2][2]]]) +\
+            matrix[0][2] * self._determinant_2x2([matrix[1][:-1], matrix[2][:-1]])
 
 
     def _direction(self, p1, p2, p3):
@@ -65,7 +65,7 @@ class Board:
                 [p1.get_y(), p2.get_y(), p3.get_y()]]
     
     
-        d = determinant_3x3(matrix)
+        d = self._determinant_3x3(matrix)
         if d > 0:
             return LEFT
         elif d == 0:
@@ -84,7 +84,7 @@ class Board:
         return reduce(lambda min_p, p: min_p if min_p.get_y() < p.get_y() else p, self._points, self._points[0])
     
     
-    def _direction_comperator(p1, p2):
+    def _direction_comperator(self, p1, p2):
         """
     :   a comperator by direction of p1--->p--->p2
     :   param p1, p2: two points to compare
@@ -96,7 +96,7 @@ class Board:
             return -1
         elif direction == SAME_LINE:
             return 0
-        else direction == RIGHT:
+        else:
             return 1
 
             
@@ -107,31 +107,45 @@ class Board:
     :           point is with minimal y
     :   rtype: list of Point
         """
-        points_without_p = list(filter(lambda p: p != p_with_min_y, self._points))
+        points_without_p = list(filter(lambda p: p != self._point_with_min_y, self._points))
         points_without_p.sort(key = cmp_to_key(self._direction_comperator))        
-        return [_self._point_with_min_y] + points_without_p
+        return [self._point_with_min_y] + points_without_p
 
 
-    def convex_hull():
+    def convex_hull(self):
         points = self._sort_points_by_angle()
         
+        stack = []
+        stack.append(points[0])
+        stack.append(points[1])
         
+        for i in range(2, len(points)):
+            while self._direction(stack[-2], stack[-1], points[i]) == LEFT:
+                stack.pop(-1)
+            
+            stack.append(points[i])
         
+        return stack
         
-        
-
 def main():
     
-
-'''
-    # left (slide 9)
-    print(direction(Point(1, 2), Point(7, 1), Point(4, 7)))
+    q1 = Point(11, 2)
+    q2 = Point(4, 10)
+    q3 = Point(7, 18)
+    q4 = Point(11, 10)
+    q5 = Point(12, 14)
+    q6 = Point(17, 15)
+    q7 = Point(21, 20)
+    q8 = Point(21, 14)
+    q9 = Point(20, 10)
+    q10 = Point(18, 6)
+    q11 = Point(23, 4)
     
-    # right (slide 21 after my change)
-    print(direction(Point(0, 0), Point(0, 1), Point(1, 0)))
+    board = Board([q11, q1, q10, q2, q9, q3, q8, q4, q7, q5, q6])
     
-    # same line
-    print(direction(Point(1, 1), Point(2, 2), Point(3, 3)))
-'''    
+    c_h = board.convex_hull()
+    for p in c_h:
+        print(p)
+    
 if __name__ == "__main__":
     main()
